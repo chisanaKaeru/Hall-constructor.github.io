c88a6e0f2ac4fd6a59edae1ab883cadb165cd47c
chisanaKaeru <pasha260777@gmail.com>
2022-12-19 11:43:16 +0300

    new version

--- javascript/main.js
+++ javascript/main.js
@@ -1,102 +1,146 @@
 //input max 60
-function inputDataSystem(item) {
-  var id = item.id
-  if (document.getElementById(id).value <= 0) {
-    document.getElementById(id).value = 1;
-  } else {
-    if (document.getElementById(id).value >= 60) {
-      document.getElementById(id).value = 60;
-    }
+function inputNumber(object) {
+  if (object.value > 60) {
+    object.value = 60;
   }
-  let valueSystem = document.getElementById(id).value;
-  return valueSystem;
+  else if (object.value < 1) {
+    object.value = 1;
+  }
+  localStorage.setItem(object.id, object.value);
 }
 
 //select change img
 function changePump() {
-  let value = document.getElementById("pump").value;
-  if (value == 1) {
-    document.getElementById("img_pump").setAttribute("style", "background-image: url(image/vacuum-pump.jpg);");
-  } else if (value == 2) {
-    document.getElementById("img_pump").setAttribute("style", "background-image: url(image/vacuum-pump.jpg);");
-  } else if (value == 3){
-    document.getElementById("img_pump").setAttribute("style", "background-image: url(image/vacuum-pump.jpg);");
+  let object = document.getElementById("pump");
+  if (object.value == "Полиэфир") {
+    document.getElementById("img-pump").setAttribute("src", "image/vacuum-pump.jpg");
+  } else if (object.value == "GEA") {
+    document.getElementById("img-pump").setAttribute("src", "http://www.rosphoto.com/images/u/articles/1510/7_5.jpg");
+  } else if (object.value == "CTA"){
+    document.getElementById("img-pump").setAttribute("src", "https://www.interfax.ru/ftproot/photos/photostory/2022/04/29/week/week7_1100.jpg");
   }
+  localStorage.setItem(object.id, object.value);
 }
 
 //select change img
-function changeWaterHeater() {
-  let value = document.getElementById("water_heater").value;
-  if (value == 1) {
-    document.getElementById("img_water_heater").setAttribute("style", "background-image: url(https://www.travelwisconsin.com/uploads/blog/e2/e2ccbd6f-1070-4272-889b-cc7720bd664d-farm-scene-with-cows.jpg);");
-  } else if (value == 2) {
-    document.getElementById("img_water_heater").setAttribute("style", "background-image: url(http://www.rosphoto.com/images/u/articles/1510/7_5.jpg);");
-  } else if (value == 3){
-    document.getElementById("img_water_heater").setAttribute("style", "background-image: url(https://www.interfax.ru/ftproot/photos/photostory/2022/04/29/week/week7_1100.jpg);");
+function changeIAAC() {
+  let object = document.getElementById("iaac");
+  if (object.value == "Версия 1") {
+    document.getElementById("img-iaac").setAttribute("src", "image/vacuum-pump.jpg");
+  } else if (object.value == "Версия 3") {
+    document.getElementById("img-iaac").setAttribute("src", "http://www.rosphoto.com/images/u/articles/1510/7_5.jpg");
+  } else if (object.value == "Версия 2"){
+    document.getElementById("img-iaac").setAttribute("src", "https://www.interfax.ru/ftproot/photos/photostory/2022/04/29/week/week7_1100.jpg");
   }
+  localStorage.setItem(object.id, object.value);
 }
 
-//select change img
-function changeCompressor() {
-  let value = document.getElementById("compressor").value;
-  if (value == 1) {
-    document.getElementById("img_compressor").setAttribute("style", "background-image: url(https://www.travelwisconsin.com/uploads/blog/e2/e2ccbd6f-1070-4272-889b-cc7720bd664d-farm-scene-with-cows.jpg);");
-  } else if (value == 2) {
-    document.getElementById("img_compressor").setAttribute("style", "background-image: url(http://www.rosphoto.com/images/u/articles/1510/7_5.jpg);");
-  }
-}
-
-//read data in localStorage
-function getValueCheckbox(checkbox){
-  if (checkbox.checked == true) {
-    localStorage.setItem(checkbox.id, checkbox.value);
+function CheckboxButton(object){
+  if (object.checked == true) {
+    localStorage.setItem(object.id, object.value);
   } else {
-    localStorage.removeItem(checkbox.id);
+    localStorage.removeItem(object.id);
   }
 }
 
-function milking_parlor() {
-  let value = document.querySelector('input[name="milking-parlor"]:checked').value;
-  localStorage.setItem("milking-parlor", value);
-}
-
-function milk_pipeline() {
-  let value = document.querySelector('input[name="milk-pipeline"]:checked').value;
-  localStorage.setItem("milk-pipeline", value);
-}
-
-function milking_system() {
-  let value = document.querySelector('input[name="milking-system"]:checked').value;
-  localStorage.setItem("milking-system", value);
-}
-
-function hanging_part() {
-  let value = document.querySelector('input[name="hanging-part"]:checked').value;
-  localStorage.setItem("hanging-part", value);
+function RadioButton(object){
+  localStorage.setItem(object.name, object.value);
+  localStorage.setItem(object.name+"-id", object.id);
 }
 
 function setCheckedPage(){
   if (localStorage.getItem("milking-parlor") != null) {
     document.getElementById("page1").setAttribute("class", "button-page true");
   }
-  for (let index = 1; index <= 5; index++) {
-    if (localStorage.getItem("option_pit" + index) != null && localStorage.getItem("input_pit" + index) != null) {
-      document.getElementById("page2").setAttribute("class", "button-page true");
-    }
+  ///
+  if (localStorage.getItem("pump") != null) {
+    document.getElementById("page3").setAttribute("class", "button-page true");
   }
-  for (let index = 1; index <= 5; index++) {
-    if (localStorage.getItem("option_pump" + index) != null) {
-      document.getElementById("page3").setAttribute("class", "button-page true");
-    }
-  } 
   if (localStorage.getItem("milk-pipeline") != null) {
     document.getElementById("page4").setAttribute("class", "button-page true");
   }
   if (localStorage.getItem("milking-system") != null) {
     document.getElementById("page5").setAttribute("class", "button-page true");
   }
+  if (localStorage.getItem("iaac") != null && localStorage.getItem("iaac-input") != null) {
+    document.getElementById("page6").setAttribute("class", "button-page true");
+  }
   if (localStorage.getItem("hanging-part") != null) {
     document.getElementById("page7").setAttribute("class", "button-page true");
+  }
+  if (localStorage.getItem("washing-pad") != null) {
+    document.getElementById("page8").setAttribute("class", "button-page true");
+  }
+  for (let index = 1; index < 6; index++) {
+    if (localStorage.getItem("equipment"+index) != null) {
+      document.getElementById("page9").setAttribute("class", "button-page true");
+      break;
+    }
+  }
+}
+
+function setCheckedInputParlor(){
+  if (localStorage.getItem("milking-parlor-id") != null) {
+    document.getElementById(localStorage.getItem("milking-parlor-id")).checked = true;
+  }
+}
+
+function setDataPump(){
+  if (localStorage.getItem("pump") != null) {
+    document.getElementById("pump").value = localStorage.getItem("pump");
+    changePump();
+  }
+}
+
+function setCheckedInputPipeline(){
+  if (localStorage.getItem("milk-pipeline-id") != null) {
+    document.getElementById(localStorage.getItem("milk-pipeline-id")).checked = true;
+  }
+}
+
+function setCheckedInputSystem(){
+  if (localStorage.getItem("milking-system-id") != null) {
+    document.getElementById(localStorage.getItem("milking-system-id")).checked = true;
+  }
+}
+
+function setDataIAAC(){
+  if (localStorage.getItem("iaac") != null) {
+    document.getElementById("iaac").value = localStorage.getItem("iaac");
+    changeIAAC();
+  }
+  if (localStorage.getItem("iaac-input") != null) {
+    document.getElementById("iaac-input").value = localStorage.getItem("iaac-input");
+  }
+}
+
+function setCheckedInputPart(){
+  if (localStorage.getItem("hanging-part-id") != null) {
+    document.getElementById(localStorage.getItem("hanging-part-id")).checked = true;
+  }
+}
+
+function setCheckedInputPad(){
+  if (localStorage.getItem("washing-pad-id") != null) {
+    document.getElementById(localStorage.getItem("washing-pad-id")).checked = true;
+  }
+}
+
+function setCheckedInpuEequipment(){
+  if (localStorage.getItem("equipment1") != null) {
+    document.getElementById("equipment1").checked = true;
+  }
+  if (localStorage.getItem("equipment2") != null) {
+    document.getElementById("equipment2").checked = true;
+  }
+  if (localStorage.getItem("equipment3") != null) {
+    document.getElementById("equipment3").checked = true;
+  }
+  if (localStorage.getItem("equipment4") != null) {
+    document.getElementById("equipment4").checked = true;
+  }
+  if (localStorage.getItem("equipment5") != null) {
+    document.getElementById("equipment5").checked = true;
   }
 }
 
--- javascript/pump.js
+++ javascript/pump.js
@@ -3,6 +3,7 @@ let pump = 0;
 function addPump() {
   if (pump<=4) {
     pump++;
+    localStorage.setItem("pumpLi", pump);
 
     let list_pump = document.getElementById("list_pump");
     let li = document.createElement("li");
@@ -24,6 +25,7 @@ function addPump() {
       input.setAttribute("type", "checkbox");
       input.setAttribute("name", "washing-pad");
       input.setAttribute("id", "pad"+pump);
+      input.setAttribute("onchange", "switchPump(this)");
 
       let span = document.createElement("span");
       span.setAttribute("class", "slider round");
@@ -46,6 +48,7 @@ function addPump() {
       let input = document.createElement("input");
       input.setAttribute("type", "number");
       input.setAttribute("id", "textbox"+pump);
+      input.setAttribute("onchange", "inputNumberPump(this)");
 
       div.append(liText, input);
       return div;
@@ -62,13 +65,74 @@ function addPump() {
 function deletePump() {
   var lu = document.getElementById('list_pump');
   lu.removeChild(lu.lastChild);
+  localStorage.removeItem("pad"+pump);
+  localStorage.removeItem("textbox"+pump);
   pump--;
+  localStorage.setItem("pumpLi", pump);
   if (pump <= 0) {
     pump = 0;
+    localStorage.removeItem("pumpLi");
   }
 }
 
 function selectData(item) {
   let value = document.getElementById(item.id).value;
   localStorage.setItem(item.id, value);
+}
+
+function switchPump(object){
+  if (object.checked == true) {
+    localStorage.setItem(object.id, object.value);
+  } else {
+    localStorage.removeItem(object.id);
+  }
+}
+
+function inputNumberPump(object) {
+  if (object.value > 60) {
+    object.value = 60;
+  }
+  else if (object.value < 1) {
+    object.value = 1;
+  }
+  localStorage.setItem(object.id, object.value);
+}
+
+function setLi(){
+  let number = localStorage.getItem("pumpLi");
+  for (let index = 1; index <= number; index++) {
+    addPump();
+  }
+
+  if (localStorage.getItem("pad1") != null) {
+    document.getElementById("pad1").checked = true;
+  }
+  if (localStorage.getItem("pad2") != null) {
+    document.getElementById("pad2").checked = true;
+  }
+  if (localStorage.getItem("pad3") != null) {
+    document.getElementById("pad3").checked = true;
+  }
+  if (localStorage.getItem("pad4") != null) {
+    document.getElementById("pad4").checked = true;
+  }
+  if (localStorage.getItem("pad5") != null) {
+    document.getElementById("pad5").checked = true;
+  }
+
+  if (localStorage.getItem("textbox1") != null) {
+    document.getElementById("textbox1").value = localStorage.getItem("textbox1");
+  }
+  if (localStorage.getItem("textbox2") != null) {
+    document.getElementById("textbox2").value = localStorage.getItem("textbox2");
+  }
+  if (localStorage.getItem("textbox3") != null) {
+    document.getElementById("textbox3").value = localStorage.getItem("textbox3");
+  }
+  if (localStorage.getItem("ptextbox") != null) {
+    document.getElementById("textbox4").value = localStorage.getItem("textbox4");
+  }
+  if (localStorage.getItem("textbox5") != null) {
+    document.getElementById("textbox5").value = localStorage.getItem("textbox5");
+  }
 }
